b03cecf53d6ca436da93f16f9db19d777717d6ca
Juan Serrano <juansr1216@gmail.com>
2026-06-09 21:31:59 +0100

    Added Gameboard and initial logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player.jsx"
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
           <Player name="Player 1" symbol="X" />
           <Player name="Player 2" symbol="O" />
         </ol>
+          <GameBoard />
       </div>
     </main>
   )
